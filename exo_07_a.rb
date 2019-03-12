8f2dc5e9328c514b580dba9a8bbbacccc145ddfd
caro <carolina.ferreira131@gmail.com>
2019-03-12 14:43:57 +0100

    fini ex jusqu'au 7b

---
+++ exo_07_a.rb
@@ -0,0 +1,3 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp #gets lit une chaîne de caractère venant de l'utilisateur, chomp élimine le retour à la ligne (touche enter)
+puts user_name
