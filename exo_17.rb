0f7e369ff56b90b7e762384fa8bb7042687edf8a
caro <carolina.ferreira131@gmail.com>
2019-03-13 10:11:36 +0100

    exo 17 fini

--- exo_17.rb
+++ exo_17.rb
@@ -5,12 +5,10 @@ birthyear = user_birthyear.to_i
 birthy = birthyear
 while birthyear <= 2017
   year = birthyear += 1
-  puts year - 1
   age = (birthyear - 1)- birthy
+  if (2017 - (birthyear - 1)) == age
+    puts "Il y a #{2017 - (birthyear - 1)} ans, tu avais la moitié de l'age que tu as aujourd'hui."
+  else
   puts "Il y a #{2017 - (birthyear - 1)} ans, tu avais #{age} ans"
-    while (2017 - (birthyear - 1)) == age
-      year = birthyear += 1
-      age = (birthyear - 1)- birthy
-      puts "Il y a #{2017 - (birthyear - 1)} ans, tu avais la moitié de l'age que tu as aujourd'hui."
     end
 end
